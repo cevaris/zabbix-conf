f4da697adeec5b2390305354f47f83ad1aa80009
Cevaris <cevaris@gmail.com>
2014-07-16 17:04:02 -0600

    can query zabbix

--- lib/zabbix_conf.rb
+++ lib/zabbix_conf.rb
@@ -1,5 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require "zabbixapi"
+
 require "zabbix_conf/version"
 
-module ZabbixConf
-  # Your code goes here...
-end
+
+HOST = 'zb00.cec.mltn.local'
+URL  = "http://#{HOST}/zabbix/api_jsonrpc.php"
+
+puts URL
+
+zbx = ZabbixApi.connect(
+  :url => URL,
+  :user => 'admin',
+  :password => 'zabbix'
+)
+puts zbx.inspect
+
+# module ZabbixConf
+#   # Your code goes here...
+# end
