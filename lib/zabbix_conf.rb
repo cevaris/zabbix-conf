1239d465906924ee0c1fa2ee8884cc52bd5dc9bf
Cevaris <cevaris@gmail.com>
2014-07-16 17:22:39 -0600

    can get/create host groups

--- lib/zabbix_conf.rb
+++ lib/zabbix_conf.rb
@@ -11,12 +11,32 @@ URL  = "http://#{HOST}/zabbix/api_jsonrpc.php"
 
 puts URL
 
-zbx = ZabbixApi.connect(
+
+$zbx = ZabbixApi.connect(
   :url => URL,
   :user => 'admin',
   :password => 'zabbix'
 )
-puts zbx.inspect
+
+
+# Gets or Creates a Hostgroup
+# Params:
+# +name+:: hostname to be created and/or retrieved
+def get_or_create_hostgroup( name )
+    hostgroup = $zbx.hostgroups.get_id(:name => name)
+    unless hostgroup
+        hostgroup = $zbx.hostgroups.create(:name => name)
+    end
+    hostgroup
+end
+
+hostgroup_name = 'testGroup2'
+puts get_or_create_hostgroup(hostgroup_name)
+
+# test = zbx.hostgroups.get_id(:name => "hostgroup")
+# puts test.inspect
+# hostg = zbx.hostgroups.create(:name => 'testGroup')
+# puts hostg.inspect
 
 # module ZabbixConf
 #   # Your code goes here...
