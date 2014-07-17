52dbc312cdc9ce0c7b686bfa666fbebd5ff65c79
Cevaris <cevaris@gmail.com>
2014-07-16 19:37:50 -0600

    found built in zabbixapi function hostgroups.get_or_create

--- lib/zabbix_conf.rb
+++ lib/zabbix_conf.rb
@@ -5,38 +5,23 @@ require "zabbixapi"
 
 require "zabbix_conf/version"
 
+# sync global stdout
+STDOUT.sync = true
+
 
 HOST = 'zb00.cec.mltn.local'
 URL  = "http://#{HOST}/zabbix/api_jsonrpc.php"
-
-puts URL
-
-
+print "Connecting to #{URL}"
 $zbx = ZabbixApi.connect(
   :url => URL,
   :user => 'admin',
   :password => 'zabbix'
 )
+puts "...connected"
 
+hostgroup_name = 'testGroup'
+puts $zbx.hostgroups.get_or_create(:name => hostgroup_name)
 
-# Gets or Creates a Hostgroup
-# Params:
-# +name+:: hostname to be created and/or retrieved
-def get_or_create_hostgroup( name )
-    hostgroup = $zbx.hostgroups.get_id(:name => name)
-    unless hostgroup
-        hostgroup = $zbx.hostgroups.create(:name => name)
-    end
-    hostgroup
-end
-
-hostgroup_name = 'testGroup2'
-puts get_or_create_hostgroup(hostgroup_name)
-
-# test = zbx.hostgroups.get_id(:name => "hostgroup")
-# puts test.inspect
-# hostg = zbx.hostgroups.create(:name => 'testGroup')
-# puts hostg.inspect
 
 # module ZabbixConf
 #   # Your code goes here...
